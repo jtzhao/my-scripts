3f69fc37af4bed35e620da6e8b555075bcb76fe2
Nathan Zhao <jtzhao@suse.com>
2016-01-19 17:43:34 +0800

    [openqa runner] Update

--- openqa-runner/openqa_runner.py
+++ openqa-runner/openqa_runner.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
+import datetime
 import fcntl
 import os
 import select
 import subprocess
 import threading
+import random
 import re
 import time
+import logging
 
 
-class ScreenProc(subprocess.Popen):
-    def __init__(self, name, pid):
-        self.name   = name
-        self.buf    = []
-        cmd = 'screen -r %d' % (pid)
-        super(ScreenProc, self).__init__(cmd, shell=True, stdin=None,
-                                        stdout=subprocess.PIPE,
-                                        stderr=subprocess.STDOUT)
+class MyPopen(subprocess.Popen):
+    def __init__(self, name, cmd):
+        self.name = name
+        super(self.__class__, self).__init__(cmd, shell=True, stdin=None,
+                                            stdout=subprocess.PIPE,
+                                            stderr=subprocess.STDOUT)
 
 
 class OpenqaRunner(object):
@@ -23,83 +24,85 @@ class OpenqaRunner(object):
     def __init__(self, run_script):
         self.script = run_script
         self.name = re.sub(r'-run$', '', os.path.basename(self.script))
-        self.epoll = select.epoll()
-        self.proc = None
-        self.ret = None
+        self.proc = None                # Subprocess of 'screen -r <pid>'
 
     def __del__(self):
         pass
 
-    def proc_list(self):
-        output = subprocess.check_output('screen -list',
-                                        shell=True)
-        lst = re.findall(r'(\d+)\.([^\s]+)\s*\((\w+)\)', output)
-        return lst
+    def heartbeat(self):
+        heart_rate = random.random() * 40 + 60
+        msg = "HR: %.7fbpm" % (heart_rate)
+        return msg
 
     def trigger(self):
         subprocess.check_call(self.script,
                                 shell=True)
         time.sleep(5)
 
-    def get_proc_ret(self):
-        self.ret = self.proc.poll()
+    def list_proc(self):
+        def _filter_proc_list(item):
+            pid, name, status = item
+            if 'hamsta' in name or status != 'Detached':
+                return False
+            return True
 
-    def proc_fetch(self):
-        lst = self.proc_list()
-        for pid, name, status in lst:
+        def _pid_to_int(item):
+            pid, name, status = item
             pid = int(pid)
-            if status == 'Attached' or self.name in name
-                                    or 'hamsta' in name:
+            return pid, name
+
+        proc = MyPopen('list-proc', 'screen -list')
+        ret = proc.wait()
+        output = proc.stdout.read()
+        lst = re.findall(r'(\d+)\.([^\s]+)\s*\((\w+)\)', output)
+        lst = filter(_filter_proc_list, lst)
+        lst = map(_pid_to_int, lst)
+        return lst
+
+    def testrun_finished(self, lst):
+        for pid, name in lst:
+            if name.endswith("-%s" % (self.name)):
+                return False
+        return True
+
+    def get_test_proc(self, lst):
+        for pid, name in lst:
+            if name.endswith("-%s" % (self.name)):
                 continue
-            self.proc = ScreenProc(name, pid)
-            break
-        assert self.proc is not None, "Failed to get test process"
-        # Set non-block flag
-        fl = fcntl.fcntl(self.proc.stdout.fileno())
-        fcntl.fcntl(self.proc.stdout.fileno(), fcntl.F_SETFL, fl | os.O_NONBLOCK)
-        # Register fd for epoll
-        self.epoll.register(self.proc.stdout.fileno(), select.EPOLLIN)
+            return pid, name
+        return None, None
 
-    def proc_remove(self):
-        if self.proc is None:
-            return
-        # Unregister fd for epoll
-        self.epoll.unregister(self.proc.stdout.fileno())
-        # Try to get return code
-        ret = self.proc.poll()
-        if ret is None:
-            self.proc.kill()
+    def monitor_test_proc(self, pid, name):
+        self.proc = MyPopen(name, 'screen -r %d' % (pid))
+        self.timestamp = datetime.datetime.now()
 
-    def proc_readlines(self):
-        try:
-            line = self.proc.stdout.readline()
-            while len(line) != 0:
-                self.proc.buf.append(line)
-                line = self.proc.stdout.readline()
-        except IOError, e:
-            pass
+    def test_proc_finished(self):
+        if self.proc.poll() is None:
+            return False
+        return True
 
-    def proc_exited(self):
-        self.proc_get_ret
-
-    def main_loop(self):
-        while len(self.proc_list()) != 0:
-            try:
-                self.proc_fetch()
-            except AssertionError, e:
-                time.sleep(5)
-                continue
-            events = self.epoll.poll(30)
-            for fd, event in events:
-                assert fd == self.proc.stdout.fileno(), "Fatal error: fd mismatch"
-                self.proc_readlines()
-            # If proc exited, remove it
-            if self.proc.poll() is not None:
-
-            # TODO: refresh ui
-            for proc in self.procs:
-                print '=' * 40 + proc.name + '=' * 40
-                print '\n'.join(proc.buf)
+    def main_loop(self, interval=3):
+        lst = self.list_proc()
+        while self.testrun_finished(lst) == False:
+            if self.proc is not None:
+                time.sleep(interval)
+                td = datetime.datetime.now() - self.timestamp
+                if self.proc.poll() is None:
+                    logging.info('"%s" running for %ds(%s)' % (self.proc.name, td.seconds, self.heartbeat()))
+                else:
+                    logging.info('"%s" finished in %.1fm(%s)' % (self.proc.name, td.seconds/60.0, self.heartbeat()))
+                    self.proc = None
+                    self.timestamp = None
+            if self.proc is None:
+                pid, name = self.get_test_proc(lst)
+                if pid is None:
+                    logging.info('No test process found(%s)' % (self.heartbeat()))
+                    time.sleep(10)
+                else:
+                    logging.info('Test process found: %d.%s(%s)' % (pid, name, self.heartbeat()))
+                    self.monitor_test_proc(pid, name)
+            lst = self.list_proc()
+        logging.info('Testrun finished(%s)' % (self.heartbeat()))
 
     def reset(self):
         subprocess.check_call('/usr/share/qa/qaset/qaset reset',
@@ -108,9 +111,13 @@ class OpenqaRunner(object):
     def run(self):
         self.reset()
         self.trigger()
-        self.start_monitor()
         self.main_loop()
-        self.stop_monitor()
 
-runner = OpenqaRunner('/usr/share/qa/qaset/run/kernel-all-run')
-runner.run()
+
+if __name__ == '__main__':
+    random.seed()
+    logging.basicConfig(format='\r[%(asctime)s] %(levelname)s: %(message)s',
+                        datefmt='%H:%M:%S',
+                        level=logging.INFO)
+    runner = OpenqaRunner('/usr/share/qa/qaset/run/kernel-all-run')
+    runner.run()
