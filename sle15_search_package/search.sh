579ac78462eab6e0d2d1b477f920e2c31e9f717f
Nathan Zhao <jtzhao@suse.com>
2017-08-29 14:47:49 +0800

    update

---
+++ sle15_search_package/search.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+function search_repo()
+{
+    local repo="$1"
+    local arch="$2"
+    local pattern="$3"
+    for suffix in $arch noarch; do
+        local url="$repo/$arch/DVD1/$suffix/"
+        local output=$(curl -s "$url" | grep -oP '(?<=")[^"]+\.rpm(?=")' | sort -h | uniq | grep -i "$pattern")
+        if [[ -n "$output" ]]; then
+            echo "[$url]"
+            echo -e "$output\n"
+        fi
+    done
+}
+
+function usage()
+{
+    echo "Usage: $0 URL PRODUCT PACKAGE [VERSION] [ARCH]" 1>&2
+}
+
+if [[ $# -lt 3 ]]; then
+    usage
+    exit 255
+fi
+
+URL="$1"
+PRODUCT="$2"
+PACKAGE="$3"
+VERSION="$4"
+[[ -z "$VERSION" ]] && VERSION="LATEST"
+ARCH="$5"
+[[ -z "$ARCH" ]] && ARCH="x86_64"
+
+# Check Leanos
+search_repo "$URL/$PRODUCT-Leanos-$VERSION" $ARCH $PACKAGE
+# Check Modules
+MODULES=$(curl -s "$URL" | grep -oP "$PRODUCT-Module-[\w\-]+-$VERSION" | sort -h | uniq)
+for module in $MODULES; do
+    search_repo "$URL/$module" $ARCH $PACKAGE
+done
